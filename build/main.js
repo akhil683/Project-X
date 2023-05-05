2135bed488a70e6d10616d88e9738fb9b9f9a1f7
akhil <akhilpalsra@gmail.com>
2023-05-06 00:21:37 +0530

    footer , filter updated

--- build/main.js
+++ build/main.js
@@ -1,38 +1,69 @@
 
-// const travelling = document.querySelector("#Travelling");
-// const travel = document.querySelector("#travel");
-
-// const hide = () => {
-//     travel.classList.toggle("flex");
-//     travel.classList.toggle("hidden");
-// }
-// travelling.addEventListener('click', hide)
-
-let travel = document.querySelector(".travel");
-let tips = document.querySelector(".tips");
-let tech = document.querySelector(".tech");
+let travel = document.querySelectorAll(".travel");
+let tips = document.querySelectorAll(".tips");
+let tech = document.querySelectorAll(".tech");
 
 let travelling = document.querySelector("#Travelling");
 let tipstricks = document.querySelector("#Tipstricks");
 let technology = document.querySelector("#Technology");
+let all = document.querySelector("#all");
+
+all.addEventListener('click', () => {
+    tech.forEach((tech) => {
+        tech.classList.remove('hidden');
+        tech.classList.add('flex');
+    })
+   tips.forEach((tips) => {
+        tips.classList.add('flex');
+        tips.classList.remove('hidden');
+   })
+   travel.forEach((travel) => {
+        travel.classList.remove('hidden');
+        travel.classList.add('flex');
+   })
+})
 
 travelling.addEventListener('click', () => {
-    tech.classList.toggle('flex');
-    tech.classList.toggle('hidden');
-    tips.classList.toggle('hidden');
-    tips.classList.toggle('flex');
+    tech.forEach((tech) => {
+        tech.classList.remove('flex');
+        tech.classList.add('hidden');
+    })
+   tips.forEach((tips) => {
+        tips.classList.add('hidden');
+        tips.classList.remove('flex');
+   })
+   travel.forEach((travel) => {
+        travel.classList.remove('hidden');
+        travel.classList.add('flex');
+   })
 } );
 
 tipstricks.addEventListener('click', () => {
-        tech.classList.toggle('flex');
-        travel.classList.toggle('flex');
-        travel.classList.toggle('hidden');
-        tech.classList.toggle('hidden');
-    });
+    tech.forEach((tech) => {
+        tech.classList.remove('flex');
+        tech.classList.add('hidden');
+    })
+   travel.forEach((travel) => {
+       travel.classList.remove('flex');
+       travel.classList.add('hidden');
+   })
+   tips.forEach((tips) => {
+        tips.classList.remove('hidden');
+        tips.classList.add('flex');
+   })
+});
 
-technology.toggleEventListener('click', () => {
-        travel.classList.toogle('flex')
-        travel.classList.toggle('hidden');
-        tips.classList.toggle('flex');
-        tips.classList.toggle('hidden');
-    });
+technology.addEventListener('click', () => {
+    tips.forEach((tips) => {
+        tips.classList.remove('flex');
+        tips.classList.add('hidden');
+    })
+   travel.forEach((travel) => {
+       travel.classList.remove('flex');
+       travel.classList.add('hidden');
+   })
+   tech.forEach((tech) => {
+        tech.classList.remove('hidden');
+        tech.classList.add('flex');
+    })
+});
